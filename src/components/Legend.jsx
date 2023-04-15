2d74d3476291604f7a0d964c75b9ec50b882516b
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-16 03:26:29 +0800

    css fix

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -31,7 +31,7 @@ const Legend = () => {
       <div className="flex flex-col items-center">
         <h1>Start node</h1>
         <img
-          src="../../public/assets/start.png"
+          src="src/assets/start.png"
           alt=""
           className="h-5 w-5 start-node"
           draggable
@@ -41,7 +41,7 @@ const Legend = () => {
       <div className="flex flex-col items-center">
         <h1>End node</h1>
         <img
-          src="../../public/assets/end.png"
+          src="src/assets/end.png"
           alt=""
           className="h-5 w-5 start-node"
           draggable
